fe6cad8680da96a45b5a14012baf303807b365fc
HL1115 <1127570788@qq.com>
2020-03-02 14:33:51 +0800

    drawer

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React,{useState,useEffect} from 'react';
 import {StyleSheet,View,Text, Image } from 'react-native';
-import {Router, Scene, Tabs} from 'react-native-router-flux';
+import {Router, Scene, Tabs, Drawer} from 'react-native-router-flux';
 import Doc from './components/Doc';
 import Msg from './components/Msg';
 import MsgDetail from './components/MsgDetail';
 import { Grid, Icon } from '@ant-design/react-native';
+
+// 添加4.0.6版本的 react-native-router-flux
 // yarn remove react-native-router-flux
 // yarn add react-native-router-flux@4.0.6
 const styles = StyleSheet.create({
@@ -14,40 +16,54 @@ const App = () => {
 	// 实现 Tabs
 	return (
 		<Router>
-			<Scene key="root">
-				<Tabs 
-					key='tabbar'
-					hideNavBar
-					activeTintColor="red"
-					inactiveTintColor="blue"
-					tabBarStyle={{backgroundColor:'#ccc'}}
-				>
-					{/* 消息栏 */}
-					<Scene key='msg'
-						title='消息'
-						icon={
-							({focused})=><Icon 
-								color={focused?'red':'green'} 
-								name="home"
-							/>
-						}
+			<Drawer 
+				key="drawer"
+				contentComponent={()=><Text>drawer</Text>}
+				drawerIcon={()=><Icon name="menu"/>}
+				drawerWidth={400}
+			>
+				<Scene key="root">
+					<Tabs 
+						key='tabbar'
+						hideNavBar
+						activeTintColor="red"
+						inactiveTintColor="blue"
+						tabBarStyle={{backgroundColor:'#ccc'}}
 					>
-
-						<Scene key="ms" component={Msg}/>
-						<Scene 
-							key="msgdetail" 
-							hideTabBar    
-							component={MsgDetail}
-						/>
-					</Scene>
-					{/* 文档栏 */}
-					<Scene key='doc'
-						icon={({focused})=><Icon color={focused?'red':'blue'} name='home'/>}
+						{/* 消息栏 */}
+						<Scene key='msg'
+							title='消息'
+							icon={
+								({focused})=><Icon 
+									color={focused?'red':'blue'} 
+									name="home"
+								/>
+							}
 						>
-						<Scene key="docs" component={Doc}/>
-					</Scene>
-				</Tabs>
-			</Scene>
+
+							<Scene key="ms" component={Msg}/>
+							<Scene 
+								key="msgdetail" 
+								hideTabBar    
+								component={MsgDetail}
+							/>
+						</Scene>
+						{/* 文档栏 */}
+						<Scene 
+							key='doc'
+							hideDrawerButton
+							icon={({focused})=>
+								<Icon 
+									color={focused?'red':'blue'} 
+									name='file'/
+							>}
+							title="文档"
+							component={Doc}
+						/>
+						
+					</Tabs>
+				</Scene>
+			</Drawer>
 		</Router>
 	);
 };
